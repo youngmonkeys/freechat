f2567900ad08f92ba56a4b1a6090f426a56ffd05
dat <datfeed93@gmail.com>
2020-03-15 10:55:21 +0700

    fix css

--- reactjs/src/views/LoginView/LoginView.jsx
+++ reactjs/src/views/LoginView/LoginView.jsx
@@ -50,34 +50,36 @@ class LoginView extends React.Component {
     render() {
         const {username, password} = this.state;
         return (
-                <div className="login-div">
-                    <Component.ToastView />
-                    <div className="login-title">
-                        <h1 className="text-light">Login to your account </h1>
-                    </div>
-                    <div>
-                        <div className="input-group mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="basic-addon-username"><i className="icon-user"></i></span>
-                            </div>
-                            <input type="text" className="form-control" placeholder="username" aria-label="username" aria-describedby="basic-addon-username"
-                                value={username} onChange={this.onUsernameChange} onKeyDown={this.onKeyDown} />
+                <div className="login-div-wrap">
+                    <div className="login-div">
+                        <Component.ToastView />
+                        <div className="login-title">
+                            <h1 className="text-light">Login to your account </h1>
                         </div>
-                        <div className="input-group mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="basic-addon-password"><i className="icon-key"></i></span>
+                        <div>
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="basic-addon-username"><i className="icon-user"></i></span>
+                                </div>
+                                <input type="text" className="form-control" placeholder="username" aria-label="username" aria-describedby="basic-addon-username"
+                                    value={username} onChange={this.onUsernameChange} onKeyDown={this.onKeyDown} />
                             </div>
-                            <input type="password" className="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon-password" 
-                                value={password} onChange={this.onPasswordChange} onKeyDown={this.onKeyDown} />
-                        </div>
-                        <div className="login-options">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" className="styled" defaultChecked="checked" /> Remember me
-                                </label>
+                            <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text" id="basic-addon-password"><i className="icon-key"></i></span>
+                                </div>
+                                <input type="password" className="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon-password"
+                                    value={password} onChange={this.onPasswordChange} onKeyDown={this.onKeyDown} />
                             </div>
+                            <div className="login-options">
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" className="styled" defaultChecked="checked" /> Remember me
+                                    </label>
+                                </div>
+                            </div>
+                            <button className="btn btn-info btn-block" onClick={this.onLogin}>Login</button>
                         </div>
-                        <button className="btn btn-info btn-block" onClick={this.onLogin}>Login</button>
                     </div>
                 </div>
        );
