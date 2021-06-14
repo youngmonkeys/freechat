3cd61555143eceb6bdea3d811e48a9aa8d9f3509
damhaihiep <damhaihiep1996@gmail.com>
2021-06-14 18:29:23 +0700

    notify android show when offilne

--- android/app/src/main/java/com/tvd12/freechat/MyFirebaseMessagingService.kt
+++ android/app/src/main/java/com/tvd12/freechat/MyFirebaseMessagingService.kt
@@ -1,11 +1,15 @@
-package com.tvd12.freechat.firebase.cloud.message
+package com.tvd12.freechat
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import android.content.IntentFilter
+
 
 class MyFirebaseMessagingService : FirebaseMessagingService(){
 
     override fun onNewToken(token: String) {
         Log.d("TAG","The token refreshed: $token")
     }
+
+
 }
