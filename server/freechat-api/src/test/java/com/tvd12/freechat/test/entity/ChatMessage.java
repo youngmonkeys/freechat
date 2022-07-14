745b62665e56ed83f0f353b9a7edd237faead530
Dung Ta Van <itprono3@gmail.com>
2022-07-14 22:53:30 +0700

    remove jpa dependecies

--- server/freechat-api/src/test/java/com/tvd12/freechat/test/entity/ChatMessage.java
+++ server/freechat-api/src/test/java/com/tvd12/freechat/test/entity/ChatMessage.java
@@ -7,23 +7,16 @@ import com.tvd12.ezyfox.database.annotation.EzyCollection;
 import com.tvd12.freechat.common.entity.ChatEntity;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 @Setter
 @Getter
 @ToString
-@Entity(name = "message")
 @EzyCollection
 @AllArgsConstructor
 @NoArgsConstructor
 @EzyObjectBinding(subTypeClasses = {ChatEntity.class})
 public class ChatMessage {
-    @Id
     @EzyId
     private long id;
-    @Column(name = "isRead")
     private boolean read;
     private String message;
     private long channelId;
